85aaeb117228e0f5f7e4448beb7bdb73527c75a3
SergioPeignier <sergio.peignier.zapata@gmail.com>
2017-05-07 21:42:33 +0200

    update class ESN

--- ESN/ESM.py
+++ ESN/ESM.py
@@ -4,13 +4,23 @@ def sigmoid (x):
 	return 1./(1. + np.exp(-x))  - 0.5     # activation function
 def sigmoid_(x):
 	return x * (1. - x)             # derivative of sigmoid
+def tanh_(x):
+	return 1 - (np.tanh(x))**2
+def tanh(x):
+	return np.tanh(x)
+def identity(x):
+	return x
+def identity_(x):
+	return 1.
 
 np.random.seed(666)
-RESERVOIR_SIZE = 1000
+RESERVOIR_SIZE = 100
 INPUT_SIZE = 1
 OUTPUT_SIZE = 1
 STOCHASTIC_GRADIENT_DECENT_LEARNING_RATE = 0.001
-LEAKING_RATE = 0.1
+LEAKING_RATE = 0.9
+TIME_CONSTANT = 0.06
+NOISE_STRENGTH = 0.001
 class ESM:
 	def __init__(self,
 		reservoir_size = RESERVOIR_SIZE,
@@ -18,10 +28,11 @@ class ESM:
 		output_size = OUTPUT_SIZE,
 		leaking_rate = LEAKING_RATE,
 		learning_rate = STOCHASTIC_GRADIENT_DECENT_LEARNING_RATE,
-		activation_function_reservoir = sigmoid,
-		activation_function_output = sigmoid,
-		derivative_activation_function_output = sigmoid_, 
-		):
+		activation_function_reservoir = tanh,
+		activation_function_output = tanh,
+		derivative_activation_function_output = tanh_,
+		time_constant = TIME_CONSTANT,
+		noise_strength = NOISE_STRENGTH):
 		"""
 		:param reservoir_size: Size of the Echo State Network
 		:type reservoir_size: int
@@ -39,10 +50,16 @@ class ESM:
 		:type activation_function_output: function
 		:param derivative_activation_function_output: derivative of the activation function for output neurons
 		:type derivative_activation_function_output: function
+		:param time_constant: time constant integration
+		:type time_constant: float
+		:param noise_strength: noise strength
+		:type noise_strength: float
 		"""
 		# Load Parameters
 		self.leaking_rate = leaking_rate
 		self.learning_rate = learning_rate
+		self.time_constant = time_constant
+		self.noise_strength = noise_strength
 
 		# Echo State Network dimensions 
 		self.N = reservoir_size
@@ -59,7 +76,7 @@ class ESM:
 		# Weights
 		self.W = np.random.rand(self.N,self.N) - 0.5
 		rhoW = np.max(np.abs(np.linalg.eig(self.W)[0]))
-		self.W *= 1.25 / rhoW
+		self.W *= 0.2 / rhoW
 		self.W_in = np.random.rand(self.N,self.K) - 0.5
 		self.W_fb = 0*(np.random.rand(self.N,self.L) - 0.5)
 		self.W_out = np.random.rand(self.L,self.N + self.K)*0.1
@@ -89,17 +106,16 @@ class ESM:
 		if y_previous is not None:
 			self.y[:] = y_previous[:]
 
-		delta_x = self.leaking_rate * self.f( self.W_in.dot(self.u) + self.W.dot(self.x) + self.W_fb.dot(self.y) )
-		self.x *= (1.-self.leaking_rate)
+		delta_x = self.time_constant * self.f( self.W_in.dot(self.u) + self.W.dot(self.x) + self.W_fb.dot(self.y) + np.random.randn(*self.y.shape)*self.noise_strength)
+		self.x *= (1.-self.leaking_rate*self.time_constant)
 		self.x += delta_x
 		self.z[:self.K] = self.u[:]
 		self.z[self.K:] = self.x[:]
 		self._pre_y = self.W_out.dot(self.z)
 		
 		self.y = self.g(self._pre_y)
-		if y_target is not None:
-			print "o"
-			self._stochastic_gradient_decent(y_target)
+
+			
 
 	def _compute_gradient(self, y_target):
 		"""
@@ -129,11 +145,12 @@ class ESM:
 		:type y_previous: array
 		"""
 		self._update_reservoir(u = x, y_previous = y_previous, y_target = y)
+		self._stochastic_gradient_decent(y_target)
 
 	def fit(self, X, y):
 		initLen = 100
-		Z = zeros((self.K+self.N,self.X.shape[0]-initLen))
-		Y_ESM = y[initLen+1:self.X.shape[0]+1]
+		Z = np.zeros((X.shape[0]-initLen,self.z.size))
+		Y_ESM = y[initLen+1:X.shape[0]+1]
 
 		self._update_reservoir(X[0,:])
 		for i,x in enumerate(X[1:initLen,:]):
@@ -141,9 +158,24 @@ class ESM:
 		for i,x in enumerate(X[initLen:,:]):
 			self._update_reservoir(u = x, y_previous =y[i-1])
 			Z[i,:] = self.z[:]
-		X_T = X.T
-		reg = 0.0000001
-		Wout = dot( y.dot(X_T), np.linalg.inv( X.dot(X_T) + reg*np.eye(1+inSize+resSize) ) )
+		self.Wout = np.linalg.pinv(Z).dot(Y_ESM)
+		return Z,y
+
+	def predict(self,X,y):
+		Z = np.zeros((X.shape[0],self.z.size))
+		Y_predicted = []
+		for i,y_i in enumerate(y):
+			x = X[i,:]
+			self._update_reservoir(u = x, y_previous =y_i)
+			Y_predicted.append(self.y[:])
+			Z[i,:] = self.z[:]
+		for i,x in enumerate(X[y.shape[0]+1:,:]):
+			x = X[i,:]
+			self._update_reservoir(u = x)
+			print i
+			Z[i+y.shape[0],:] = self.z[:]
+			Y_predicted.append(self.y[:])
+		return Z,Y_predicted
 		
 	def predict_online(self, x=None, y = None):
 		"""
@@ -158,123 +190,44 @@ class ESM:
 	def SSE(self,y_target):
 		return np.sum(0.5*(self.y - y)**2)
 
-"""
+
+
 import matplotlib.pyplot as plt
-data = np.loadtxt('MackeyGlass_t17.txt')
-#plt.plot(data[0:1000])
-#plt.title('A sample of data')
-#plt.show()
-
-esm = ESM()
-print esm.W_in.shape
-print esm.W.shape
-print esm.W_in.mean()
-print esm.W.mean()
-
-
-import matplotlib.pyplot as plt 
-esm = ESM()
-
-X = np.ones(1000)
-X[0:100] *= 0.5
-X[100:200] *= 0.7
-X[200:300] *= 0.9
-X[400:500] *= 0.3
-X[500:600] *= 0.1
-X[600:700] *= 0.9
-X[700:800] *= 0.3
-
-Y = np.sin(np.asarray(range(X.size))*X.T)
-X = X.reshape(1000,1)
-Y = Y.reshape(1000,1)
-plt.plot(range(Y.size)[:1000], Y[:1000])
-plt.show()
-print Y
-print X
-quality = []
-ans = []
-for _ in range(100):
-	for i in range(1,100):
-		x = X[i,:]
-		y = Y[i]
-		y_prev = Y[i-1]
-		#print x,y
-		#esm.predict(x)
-		esm.fit(x,y_prev,y)
-		quality.append(esm.SSE(y))
-		ans.append(esm.y)
-	print _
-		#ans.append(esm.predict(x))
-for i in range(1,1000):
-		x = X[i,:]
-		y = Y[i-1]
-		#print x,y
-		esm.predict(x, y)
-		#else:
-		#	esm.predict(x)
-		#ans.append(esm.y)
-#plt.ylim(0.4,0.6)
-#plt.plot(range(len(ans)), ans)
-plt.plot(range(len(quality)), quality)
-plt.show()
-
-
-plt.plot(range(len(ans[:100])), ans[:100])
-plt.show()
-
-
-"""
-
 import pandas as pd
-import matplotlib.pyplot as plt
+data = pd.read_csv("../data/EEG_SAX.csv",index_col=0)
+y = (data.label - data.label.mean())*1./data.label.std()
+u = data.drop('label', 1)
+u = (u-u.mean())*1./u.std()
+_ = plt.plot(range(len(u)),u)
+_ = plt.plot(range(len(y)),y)
 
-def update_network(a,X,y,W,Wfb,f):
-	return (1-a) * X + a * f(W.dot(X) + Wfb.dot(y))
-
-
-def compute_output(X,Wout):
-	return Wout.T.dot(X)
-
-
-X = np.random.rand(20) - 0.5
-W = np.random.rand(20,20) - 0.5
-Wbf = (np.random.rand(20,1) - 0.5)
-rhoW = np.max(np.abs(np.linalg.eig(W)[0]))
-W *= 0.9 / rhoW
-
-
-X_full = np.ones((200,20))
-y = (0.5*np.sin(np.asarray(range(600)) * 0.25)).reshape(600,1)
-
-for i in range(100):
-	X = update_network(0.5,X,y[i],W,Wbf,np.tanh)
-
-for i in range(200):
-	X_full[i,:] = X
-	X = update_network(0.5,X,y[i+100],W,Wbf,np.tanh)
-
-
-Wout = np.linalg.pinv(X_full).dot(y[:200,:])
-
-plt.plot(pd.DataFrame(X_full))
+u = u.values
+y = y.values
+y = y.reshape(y.shape[0],1)
 plt.show()
 
-ans = []
-for i in range(300):
-	X = update_network(0.5,X,y[i+300-1],W,Wbf,np.tanh)
-	ans.append(compute_output(X,Wout))
+esm = ESM(reservoir_size = 100,
+	input_size = 14,
+	output_size = 1,
+	leaking_rate = 0.9,
+	learning_rate = STOCHASTIC_GRADIENT_DECENT_LEARNING_RATE,
+	activation_function_reservoir = tanh,
+	activation_function_output = identity,
+	derivative_activation_function_output = identity_,
+	time_constant = 0.06,
+	noise_strength = NOISE_STRENGTH)
 
-plt.plot(range(300),y[300:])
-plt.plot(range(300),ans)
+training_size = 1000
+X_full,Y_full = esm.fit(u[:training_size,:],y[:training_size+1])
+_=plt.plot(pd.DataFrame(X_full))
 plt.show()
-"""
+print X_full.shape
 
-
-plt.plot(range(300),y)
+plt.plot(esm.Wout)
 plt.show()
 
-for i in range(100):
-	X = update_network(0.5,X,W,np.tanh)
+X,Y = esm.predict(u[:training_size,:],y[:100])
 
-"""
-
+plt.plot(range(len(Y)),Y)
+plt.plot(range(len(Y)),y[:len(Y)])
+plt.show()
